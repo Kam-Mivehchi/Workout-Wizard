c9024c74c977da1e8e34948355643f759704322b
Ace <omarace95@gmail.com>
2021-10-28 20:48:08 -0700

    application can now look up exercises by workout and displays card with instructions

--- script.js
+++ script.js
@@ -1,12 +1,117 @@
-var absEl = document.querySelector('#Abs-Content');
-var armsEl = document.querySelector('#Arms-Content');
-var backEl = document.querySelector('#Back-Content');
-var calvesEl = document.querySelector('#Calves-Content');
-var chestEl = document.querySelector('#Chest-Content');
-var legsEl = document.querySelector('#Legs-Content');
-var shouldersEl = document.querySelector('#Shoulders-Content');
+var formEl = $('#search-form')
+var displayWorkOutsEl = document.querySelector('#display')
+var displayExerciseEl = document.querySelector('#display2')
 
-urlTest = "https://wger.de/api/v2/exerciseinfo/?language=2&limit=231";
+//Muscle Variables
+var deltEl = $('#delts')
+var bicepArmsEl = $('#bicepArms')
+var bicepLegsEl = $('#bicepLegs')
+var elbowEl = $('#elbow')
+var calvesEl = $('#calves')
+var buttEl = $('#butt')
+var latsEl = $('#lats')
+var obliqueEl = $('#oblique')
+var pecsEl = $('#pecs')
+var quadsEl = $('#quads')
+var pecs2El = $('#pecs2')
+var calvesEl = $('#calves2')
+var trapsEl = $('#traps')
+var trisEl = $('#tris')
+
+//Looks for Muscle Click
+$(document).ready(function() {
+  $('.btn-muscle').on('click', function(){
+    event.preventDefault();
+    var muscle = $(this).attr('data-id');
+    console.log(muscle);
+    urlMuscle = "https://wger.de/api/v2/exercise/?muscles="+muscle+"&language=2";
+    
+    fetch(urlMuscle, {
+      headers: {
+          'Authorization': 'Token 954c1b128bf5599c33df6960a53dc2a5d3a7b6b4'
+      }
+    })
+    .then(function (response){
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data){
+      console.log(data);
+
+        for (var i = 0; i < data.results.length; i++) {
+          console.log(data.results[i].name)
+          displayWorkOutsEl.innerHTML = displayWorkOutsEl.innerHTML+`<button data-exercise="${data.results[i].id}" class="btn btn-exercise btn-primary">${ data.results[i].name }</button>
+        `}
+
+        //Looks for Exercise
+        $(document).ready(function() {
+          $('.btn-exercise').on('click', function(){
+            event.preventDefault();
+            var exercise = $(this).attr('data-exercise');
+            console.log(muscle);
+            urlMuscle = "https://wger.de/api/v2/exerciseinfo/"+exercise+"/?language=2";
+            
+            fetch(urlMuscle, {
+              headers: {
+                  'Authorization': 'Token 954c1b128bf5599c33df6960a53dc2a5d3a7b6b4'
+              }
+            })
+            .then(function (response){
+              console.log(response);
+              return response.json();
+            })
+            .then(function (data){
+              console.log(data);
+        
+                  console.log(data.name)
+                  displayExerciseEl.innerHTML = displayExerciseEl.innerHTML+`
+                  <div class="card bg-light text-dark mb-3 p-3">
+                  <div class="card-body">
+                    <h3>${ data.name } </h3>
+                      <p><strong>Instructions</strong> ${data.description} <br>
+                  </div>
+                </div>
+              `
+            })
+          });      
+        })
+
+    })
+  });      
+})
+
+// function getExercises(){
+//   event.preventDefault();
+//   var exercise = $(this).attr('data-exercise');
+//   console.log(muscle);
+//   urlMuscle = "https://wger.de/api/v2/exerciseinfo/"+exercise+"/?language=2";
+  
+//   fetch(urlMuscle, {
+//     headers: {
+//         'Authorization': 'Token 954c1b128bf5599c33df6960a53dc2a5d3a7b6b4'
+//     }
+//   })
+//   .then(function (response){
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function (data){
+//     console.log(data);
+
+//         console.log(data.name)
+//         displayExerciseEl.innerHTML = displayExerciseEl.innerHTML+`
+//         <div class="card bg-light text-dark mb-3 p-3">
+//         <div class="card-body">
+//           <h3>${ data.name } </h3>
+//             <p><strong>Instructions</strong> ${data.description} <br>
+//         </div>
+//       </div>
+//     `
+//   })
+// };      
+
+//TEST
+urlTest = "https://wger.de/api/v2/exerciseinfo/307/?language=2";
 
 fetch(urlTest, {
     headers: {
@@ -19,71 +124,4 @@ fetch(urlTest, {
 })
 .then(function (data){
     console.log(data);
-    
-    for (var i=0;i<data.results.length;i++) {
-        //Display Abs
-        if (data.results[i].category.id == 10) {
-            absEl.innerHTML = absEl.innerHTML+`
-            <div class="card bg-light text-dark mb-3 p-3">
-              <div class="card-body">
-                <h3>${ data.results[i].name} </h3>
-              </div>
-            </div>
-          `}
-        //Display Arms
-        else if (data.results[i].category.id == 8) {
-            armsEl.innerHTML = armsEl.innerHTML+`
-            <div class="card bg-light text-dark mb-3 p-3">
-              <div class="card-body">
-                <h3>${ data.results[i].name} </h3>
-              </div>
-            </div>
-          `}
-        //Display Back
-        else if (data.results[i].category.id == 12) {
-            backEl.innerHTML = backEl.innerHTML+`
-            <div class="card bg-light text-dark mb-3 p-3">
-              <div class="card-body">
-                <h3>${ data.results[i].name} </h3>
-              </div>
-            </div>
-          `}
-        //Display Calves
-        else if (data.results[i].category.id == 14) {
-            calvesEl.innerHTML = calvesEl.innerHTML+`
-            <div class="card bg-light text-dark mb-3 p-3">
-              <div class="card-body">
-                <h3>${ data.results[i].name} </h3>
-              </div>
-            </div>
-          `}
-        //Display Chest
-        else if (data.results[i].category.id == 11) {
-            chestEl.innerHTML = chestEl.innerHTML+`
-            <div class="card bg-light text-dark mb-3 p-3">
-              <div class="card-body">
-                <h3>${ data.results[i].name} </h3>
-              </div>
-            </div>
-          `}
-        //Display Legs
-        else if (data.results[i].category.id == 9) {
-            legsEl.innerHTML = legsEl.innerHTML+`
-            <div class="card bg-light text-dark mb-3 p-3">
-              <div class="card-body">
-                <h3>${ data.results[i].name} </h3>
-              </div>
-            </div>
-          `}
-        //Display Shoulders
-        else if (data.results[i].category.id == 13) {
-            shouldersEl.innerHTML = shouldersEl.innerHTML+`
-            <div class="card bg-light text-dark mb-3 p-3">
-              <div class="card-body">
-                <h3>${ data.results[i].name} </h3>
-              </div>
-            </div>
-          `}
-    }
-});
-
+})
